c526f8da3d7b8b49f2d1b29e2f7564cbf18770db
JuanD <juanxd3009@gmail.com>
2023-10-04 16:10:51 -0500

    ruta get pokemons completo

--- api/src/controllers/getPokemons.js
+++ api/src/controllers/getPokemons.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const { obtenerInformacionPokemon } = require("../handlers/handlerPokemon");
 
 const URL = "https://pokeapi.co/api/v2/pokemon/";
 
@@ -6,10 +7,11 @@ const getPokemons = async (req, res) => {
   try {
     const { data } = await axios(URL);
     const response = data.results;
-    res.status(200).json(response);
-    if (!response) {
-      return res.status(404).json({ error: "verificar url" });
-    }
+    // console.log("response", response);
+
+    const pokemon = await obtenerInformacionPokemon(response);
+    // console.log(pokemon);
+    res.status(200).json(pokemon);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- api/src/handlers/handlerPokemon.js
+++ api/src/handlers/handlerPokemon.js
@@ -1,16 +1,48 @@
 const axios = require("axios");
 
-const URL = "https://pokeapi.co/api/v2/pokemon/";
+const obtenerInformacionPokemon = async (response) => {
+  //   console.log(response);
+  const pokemonInfoArray = [];
 
-const handlerPokemon =async (query)=>{
+  for (const pokemon of response) {
     try {
-        const { data } = await axios(URL);
-        // const pokemon
-        res.status(200).json(data);
-      } catch (error) {
-        res.status(500).json({ error: error.message });
-      }
-} 
-  
+      const response = await axios.get(pokemon.url);
+      //   const pokemonInfo = response.data;
+      pokemonInfoArray.push(response.data);
+    } catch (error) {
+      console.error(
+        `No se pudo obtener información de ${pokemon.name}: ${error.message}`
+      );
+    }
+  }
+  const pokemonForId = pokemonInfoArray.map((pok) => {
+    const pokHp = pok.stats.filter((stat) => stat.stat.name === "hp");
+    const hp = pokHp.map((stat) => stat.base_stat);
 
-module.exports = handlerPokemon;
+    const pokDefen = pok.stats.filter((stat) => stat.stat.name === "defense");
+    const def = pokDefen.map((stat) => stat.base_stat);
+
+    const pokAttack = pok.stats.filter((stat) => stat.stat.name === "attack");
+    const att = pokAttack.map((stat) => stat.base_stat);
+
+    const pokSpeed = pok.stats.filter((stat) => stat.stat.name === "speed");
+    const speed = pokSpeed.map((stat) => stat.base_stat);
+
+    const pokemonForId = {
+      id: pok.id,
+      nombre: pok.name,
+      imagen: "Aca va la imagen",
+      vida: hp[0],
+      ataque: att[0],
+      defensa: def[0],
+      velocidad: speed[0],
+      altura: pok.height,
+      peso: pok.weight,
+      //   types: pok.types.map((type) => type.type.name),
+    };
+    return pokemonForId;
+  });
+  return pokemonForId;
+};
+
+module.exports = { obtenerInformacionPokemon };
